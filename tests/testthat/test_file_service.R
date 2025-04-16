1c01700528a7941d6ad6737252906b245a09e232
Andreas Schweiger <aschweiger@datagration.com>
2025-04-16 14:15:55 +0200

    - add class ApiRequests - add class FileService - add FileService to ServiceProvider - add file service tests - update documentation

---
+++ tests/testthat/test_file_service.R
@@ -0,0 +1,37 @@
+context("FileService Tests")
+
+# Create test data
+test_data <- data.frame(entity = c("One", "Two", "Three"),
+                        x = c(1, 2, 3),
+                        name = c("This", "is", "something"))
+
+# Save data to csv file
+write.csv(test_data, "test_file.csv", row.names = FALSE)
+
+# Perform tests
+test_that("File can be uploaded", {
+  result <- sp$files$save("test_file.csv")
+
+  expect_equal(result$status_code, 200)
+})
+
+test_that("File is available on the server", {
+  file_names <- sp$files$load_names()
+
+  expect_true("test_file.csv" %in% file_names)
+})
+
+test_that("File can be downloaded", {
+  file <- sp$files$load("test_file.csv")
+
+  # Convert content to data.frame
+  data_retrieved <- read.csv(text = file)
+
+  expect_equal(data_retrieved, test_data)
+})
+
+test_that("File can be deleted", {
+  result <- sp$files$delete("test_file.csv")
+
+  expect_equal(result$status_code, 200)
+})
